210c56dcd8994434d13b445cb671f71060598bd4
Klaidas <klaidas.vysniauskas@mijora.lt>
2024-04-24 23:16:40 +0300

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CargoType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        CargoType::factory()->create([
+            CargoType::NAME => 'Autovežis',
+        ]);
+        CargoType::factory()->create([
+            CargoType::NAME => 'Vilkikas su tentine priekaba'
+        ]);
     }
 }
--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,8 @@ set -e
 echo "Preparing PHP dependencies..."
 $SCRIPT_DIR/backend.sh 'composer' 'install'
 $SCRIPT_DIR/backend.sh 'php' 'artisan' 'key:generate'
+$SCRIPT_DIR/backend.sh 'php' 'artisan' 'migrate'
+$SCRIPT_DIR/backend.sh 'php' 'artisan' 'db:seed'
 $SCRIPT_DIR/backend.sh 'npm' 'install'
 $SCRIPT_DIR/backend.sh 'npm' 'run' 'build'
 
